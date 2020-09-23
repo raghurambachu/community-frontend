c81d82bf14419b3fcc29ebace8e7bc8ee816d9c0
Raghuram <1993raghuram@gmail.com>
2020-09-23 11:27:34 +0530

    Add Register page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 // import LandingPage from "./LandingPage";
-import LoginPage from "./loginpage/LoginPage";
+// import LoginPage from "./loginpage/LoginPage";
+import RegisterPage from "./registerpage/RegisterPage";
 
 class App extends React.Component {
   render() {
     return (
       <>
         {/* <LandingPage /> */}
-        <LoginPage />
+        {/* <LoginPage /> */}
+        <RegisterPage />
       </>
     );
   }
